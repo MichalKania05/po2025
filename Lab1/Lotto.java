0c5fbceaea3f13255e5279c64039f7a9f863231f
MichalKania05 <michalkania@student.agh.edu.pl>
2025-10-08 20:12:17 +0200

    Update Lotto.java

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,16 +1,25 @@
 
 import java.util.Random;
+import java.util.HashSet; // Odrzuca duplikaty
 
-public class Lotto {
-    public static void main(String[] args){
-        
-        Random los = new Random();
-        int n1 = los.nextInt(1, 3);
-        int n2 = los.nextInt(1, 3);
-        int n3 = los.nextInt(1, 3);
-        
-        System.out.println(n1);
-        System.out.println(n2);
-        System.out.println(n3);
+public class Lotto
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> wylosowane = new HashSet<>();
+        Random losuj = new Random();
+
+        while (wylosowane.size() < 6) // Losowanie
+        {
+            int nowa = losuj.nextInt(49) + 1;
+            wylosowane.add(nowa);
+        }
+
+        System.out.println("Wylosowane liczby: "); // Wypisanie
+        for (int liczba : wylosowane)
+        {
+            System.out.print(liczba + " ");
+        }
     }
 }
+
