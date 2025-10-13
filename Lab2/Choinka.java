1cfced839686f93f44c3d68dddbec79e9bafab14
Your <you@example.com>
2025-10-13 21:14:24 +0200

    Test 2

---
+++ Lab2/Choinka.java
@@ -0,0 +1,18 @@
+
+public class Choinka
+{
+    public static void main(String[] args)
+    {
+        int rozmiar = Integer.parseInt(args[0]);
+
+        for(int i=1; i<=rozmiar; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
