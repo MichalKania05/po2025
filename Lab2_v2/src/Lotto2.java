005aae1cf98175fbf1c007d32f89b9d30a675d53
Your <you@example.com>
2025-10-14 19:44:56 +0200

    Lotto2 - do poprawienia

--- Lab2_v2/src/Lotto2.java
+++ Lab2_v2/src/Lotto2.java
@@ -10,37 +10,47 @@ public class Lotto2
     {
         Scanner wczyt = new Scanner(System.in);
         System.out.println("Podaj liczby: ");
-        int liczby1 = wczyt.nextInt();
-        int liczby2 = wczyt.nextInt();
-        int liczby3 = wczyt.nextInt();
-        int liczby4 = wczyt.nextInt();
-        int liczby5 = wczyt.nextInt();
-        int liczby6 = wczyt.nextInt();
-        int liczby_user[] = {liczby1, liczby2, liczby3, liczby4, liczby5, liczby6};
+
+        //int liczby1 = wczyt.nextInt();
+        //int liczby2 = wczyt.nextInt();
+        //int liczby3 = wczyt.nextInt();
+        //int liczby4 = wczyt.nextInt();
+        //int liczby5 = wczyt.nextInt();
+        //int liczby6 = wczyt.nextInt();
+        //int twojeliczby[] = {liczby1, liczby2, liczby3, liczby4, liczby5, liczby6};
+
+        //int[] liczby_gracz;
+        var liczby_gracz = new int[6];
+        for(int k=0 ; k<=5 ; k=k+1)
+        {
+            liczby_gracz[k] = wczyt.nextInt();
+        }
+
 
         ArrayList<Integer> wylosowane = new ArrayList<>();
         Random losuj = new Random();
 
         int zgodnosc = 0;
 
-        while (wylosowane.size() < 6) // Losowanie
+        while (wylosowane.size() < 6)
         {
             int nowa = losuj.nextInt(49) + 1;
             wylosowane.add(nowa);
             for(int k=0 ; k<=5 ; k=k+1)
             {
-                if(liczby_user[k] == nowa)
+                if(liczby_gracz[k] == nowa)
                 {
                     zgodnosc = zgodnosc + 1;
                 }
             }
         }
 
-        System.out.println("Wylosowane liczby: "); // Wypisanie
+        System.out.println("Wylosowane liczby: ");
         for (int liczba : wylosowane)
         {
-            System.out.println(liczba + " ");
+            System.out.print(liczba + " ");
         }
+        System.out.println();
 
         System.out.println("Liczba trafień: " + zgodnosc);
     }
