ca2096aa921fa9c9e4ca32c073b5e55a1aa2c9ad
Your <you@example.com>
2025-10-14 18:56:06 +0200

    reorganizacja repozytorium

---
+++ Lab2_v2/src/Choinka.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Choinka
+{
+    public static void main(String[] args)
+    {
+        Scanner wczyt = new Scanner(System.in);
+        System.out.print("Podaj rozmiar choinki: ");
+        int rozmiar = wczyt.nextInt();
+
+        for(int i=1; i<=rozmiar; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
