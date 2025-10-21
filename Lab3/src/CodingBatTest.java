936b7ab7cc1746a8d6c4f4a86e4a11bd82f07220
Your <you@example.com>
2025-10-21 19:45:45 +0200

    CodingBatTest - wszystkie testy

--- Lab3/src/CodingBatTest.java
+++ Lab3/src/CodingBatTest.java
@@ -46,9 +46,24 @@ public class CodingBatTest
         assertEquals("kit", new CodingBat().firstHalf("kitten"));
     }
 
-    /*@org.junit.Test
+    @org.junit.Test
     public void countEvens()
     {
-        assertEquals(3, new CodingBat().);
-    }*/
+        int[] cE1 = {2,1,2,3,4};
+        int[] cE2 = {2, 2, 0};
+        int[] cE3 = {1, 3, 5};
+        int[] cE4 = {};
+        int[] cE5 = {11, 9, 0, 1};
+        int[] cE6 = {2, 11, 9, 0};
+        int[] cE7 = {2};
+        int[] cE8 = {2, 5, 12};
+        assertEquals(3, new CodingBat().countEvens(cE1));
+        assertEquals(3, new CodingBat().countEvens(cE2));
+        assertEquals(0, new CodingBat().countEvens(cE3));
+        assertEquals(0, new CodingBat().countEvens(cE4));
+        assertEquals(1, new CodingBat().countEvens(cE5));
+        assertEquals(2, new CodingBat().countEvens(cE6));
+        assertEquals(1, new CodingBat().countEvens(cE7));
+        assertEquals(2, new CodingBat().countEvens(cE8));
+    }
 }
