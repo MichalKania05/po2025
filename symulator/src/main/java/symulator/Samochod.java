d2332ad3195c75b2f64ce4e8d27f6acc2de75ae3
Your <you@example.com>
2025-11-12 19:56:22 +0100

    Lab5 - implementacja nowej metody, do poprawki

--- symulator/src/main/java/symulator/Samochod.java
+++ symulator/src/main/java/symulator/Samochod.java
@@ -33,6 +33,12 @@ public class Samochod extends Pozycja
         {
             silnik.uruchom();
             stanWlaczenia = true;
+            System.out.println("Włączono silnik! ");
+        }
+
+        else
+        {
+            System.out.println("Silnik jest już włączony! ");
         }
     }
 
@@ -46,6 +52,12 @@ public class Samochod extends Pozycja
                 skrzynia.zmniejszBieg();
             }
             stanWlaczenia = false;
+            System.out.println("Wyłączono silnik! ");
+        }
+
+        else
+        {
+            System.out.println("Silnik jest już wyłączony! ");
         }
     }
 
@@ -100,4 +112,59 @@ public class Samochod extends Pozycja
     {
         return skrzynia;
     }
+
+
+    // Testowanie klasy
+        public static void main(String[] args)
+        {
+            Sprzeglo spr1 = new Sprzeglo("idk", "SP1",
+                    "Sprzeglo1", 0.005, 250);
+
+            SkrzyniaBiegow sb1 = new SkrzyniaBiegow("Skrzyniarz", "SB1",
+                    "Skrzynia1", 25, 5000, 6, spr1);
+
+            Silnik sil1 = new Silnik("Tworca Silnikow", "SIL1",
+                    "Silnik1", 10000, 50000, 8000);
+
+            Samochod sam1 = new Samochod("XYZ-1234", "Golf 5",
+                    250, sil1, sb1);
+
+
+            // TESTOWANIE
+            System.out.println("Czy samochód włączony: " + sam1.isStanWlaczenia());
+            System.out.println("Czy sprzęgło wciśnięte: " + spr1.stanSprzegla());
+            sam1.wlacz();
+            spr1.wcisnij();
+            System.out.println("Czy samochód włączony: " + sam1.isStanWlaczenia());
+            System.out.println("Czy sprzęgło wciśnięte: " + spr1.stanSprzegla());
+
+            sb1.zwiekszBieg();
+            sb1.zwiekszBieg();
+            sb1.zwiekszBieg();
+            sb1.zmniejszBieg();
+            sb1.zmniejszBieg();
+            System.out.println("Aktualny bieg: " + sb1.getAktBieg());
+            System.out.println("Aktualna prędkość: " + sam1.getAktPredkosc());
+            System.out.println("Aktualne przełożenie: " + sb1.getAktPrzelozenie());
+            sb1.zmniejszBieg();
+            sb1.zmniejszBieg();
+
+            System.out.println(sam1.getAktPozycja());
+            Pozycja cel1 = new Pozycja(4, 5);
+            sam1.jedzDo(cel1);
+            System.out.println(sam1.getAktPozycja());
+            Pozycja cel2 = new Pozycja(-3, -2);
+            sam1.jedzDo(cel2);
+            System.out.println(sam1.getAktPozycja() + "\n");
+
+            // Sprawdzenie nowej metody
+            Pozycja cel3 = new Pozycja(7, 18);
+            sam1.przemiesc(cel3, 2, 0.2);
+            System.out.println("\n");
+
+            spr1.zwolnij();
+            sam1.wylacz();
+            System.out.println("Czy samochód włączony: " + sam1.isStanWlaczenia());
+            System.out.println("Czy sprzęgło wciśnięte: " + spr1.stanSprzegla());
+        }
 }
